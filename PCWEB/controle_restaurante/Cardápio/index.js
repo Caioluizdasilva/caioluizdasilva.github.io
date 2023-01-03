a0eb3d11b8ee0bf6c7d2d0d2fcf3292e086efa95
Caio Silva <caio.silva.3@aluno.cefet-rj.br>
2023-01-03 17:23:24 -0300

    atualização_restaurante

---
+++ PCWEB/controle_restaurante/Cardápio/index.js
@@ -0,0 +1,43 @@
+window.onload = async () => {
+    await fetch('https://rafaelescalfoni.github.io/desenv_web/restaurante/items.json')
+        .then(async response => await response.json())
+        .then(async responseJSON => {
+            responseJSON.forEach((item) => {
+                createItem(item)
+            })
+        })
+}
+
+const createItem = (item) => {
+    const items = document.querySelector(".items")
+    const name = typeof item.name !== 'undefined' ? item.name : item.title
+    const prices = item.price.split('$')
+    console.log(prices)
+    const div = `<div class="col-3 mb-5">
+                    <div class="card h-100" style="width: 18rem;">
+                        <img src="${'https://raw.githubusercontent.com/rafaelescalfoni/rafaelescalfoni.github.io/main/desenv_web/restaurante/' + item.photo}" class="card-img-top" alt="${name}">
+                        <div class="card-body d-flex align-items-start flex-column">
+                            <h5 class="card-title">${name}</h5>
+                            <p class="card-text">${item.details}</p>
+                                <a href="#" class="btn btn-danger mt-auto">${item.price}</a>   
+                        </div>
+                    </div>
+                </div>`
+    items.innerHTML += div
+}
+// Passo o ID do produto atraves do metodo get para a pagina "detalhes.js"
+        var passaValor= function(ite,){
+            window.location = "detalhes.html?produto="+item;
+        }
+        
+        var links = document.getElementsByTagName('a');
+            for(var i = 0; i < links.length; i++){
+                links[i].addEventListener("click",function(){
+                    let key = this.getAttribute('key');
+                    console.log("O valor é: " + key);
+        
+                    var valorQueEuQueroPassar = key;
+                    passaValor(valorQueEuQueroPassar);
+                    return false;
+                })
+            }
